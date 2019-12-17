0dc96fdcbf27154124557177013583931f954d8e
Elliot Moule <ellimyt@live.com.au>
2019-12-17 16:40:05 +1100

    [multi-line drawing]

---
+++ 3 - drawing multi-lines/canvas-script.js
@@ -0,0 +1,15 @@
+
+window.onload = function () {
+    // Definitions
+    var canvas  = document.getElementById("multi-line-canvas");
+    var context = canvas.getContext("2d");
+    
+    // Draw line steps
+    context.beginPath();                // reset the context state
+    context.moveTo(30, 30);             // moveTo(x,y) -> Starting point of line.
+    context.lineTo(80, 80);            // line(x,y) -> end point of the line.
+    context.lineTo(130, 30);            // line(x,y) -> end point of the line.
+    context.lineTo(180, 80);            // line(x,y) -> end point of the line.
+    context.lineTo(230, 30);            // line(x,y) -> end point of the line.
+    context.stroke();                   // this draws the line.
+}
