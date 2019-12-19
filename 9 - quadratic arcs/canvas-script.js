098b10924937edc3e01657c96bcbabddb62d7cfc
Elliot Moule <ellimyt@live.com.au>
2019-12-19 15:43:16 +1100

    [quadratic arcs]

---
+++ 9 - quadratic arcs/canvas-script.js
@@ -0,0 +1,13 @@
+
+window.onload = function () {
+    // Definitions
+    var canvas  = document.getElementById("quadratic-arcs-canvas");
+    var context = canvas.getContext("2d");
+
+    context.beginPath();
+    context.strokeStyle = "red";
+    context.lineWidth = 10;
+    context.moveTo(200, 250);
+    context.quadraticCurveTo(500, 110, 400, 250);
+    context.stroke();
+}
