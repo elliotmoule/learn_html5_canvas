0434a8190fd4893430c5c376f99d0cc3ada69e99
Elliot Moule <ellimyt@live.com.au>
2019-12-19 15:54:42 +1100

    [bezier arcs]

--- 10 - bezier arcs/canvas-script.js
+++ 10 - bezier arcs/canvas-script.js
@@ -1,16 +1,15 @@
 
 window.onload = function () {
     // Definitions
-    var canvas  = document.getElementById("quadratic-arcs-canvas");
+    var canvas  = document.getElementById("bezier-arcs-canvas");
     var context = canvas.getContext("2d");
 
+    // context.bezierCurveTo(controlX1, controlY1, controlX2, controlY2, endX, endY)
 
-    // context.quadraticCurveTo(controlX, controlY, endX, endY)
-    
     context.beginPath();
     context.strokeStyle = "red";
     context.lineWidth = 10;
     context.moveTo(200, 250);
-    context.quadraticCurveTo(500, 110, 400, 250);
+    context.bezierCurveTo(200, 10, 50, 150, 400, 250);
     context.stroke();
 }
