aea3c0720c355192b32091333e983b8576b875be
Elliot Moule <ellimyt@live.com.au>
2019-12-19 16:51:49 +1100

    [circular objects]

---
+++ 14 - drawing circular objects/canvas-script.js
@@ -0,0 +1,18 @@
+
+window.onload = function () {
+    // Definitions
+    var canvas  = document.getElementById("circular-objects-canvas");
+    var context = canvas.getContext("2d");
+
+    var radian = Math.PI / 180;
+
+    // context.arc(centerX, centerY, radius, startAngle, endAngle, antiClockwiseDirection);
+
+    context.beginPath();
+    context.strokeStyle = "green";
+    context.lineWidth = 10;
+    context.fillStyle = "green";
+    context.arc(450, 300, 150, 135 * radian, 360 * radian, false);
+    context.stroke();
+    context.fill();
+}
