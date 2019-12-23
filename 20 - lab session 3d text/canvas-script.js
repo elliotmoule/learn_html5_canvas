b8f7f657d3c405b113bdec8f45ee86c40e5eb9f7
Elliot Moule <ellimyt@live.com.au>
2019-12-23 11:41:11 +1100

    [3d text]

---
+++ 20 - lab session 3d text/canvas-script.js
@@ -0,0 +1,21 @@
+
+window.onload = function () {
+    // Definitions
+    var canvas  = document.getElementById("three-dimentional-text-canvas");
+    var context = canvas.getContext("2d");
+
+    draw3DText("This text will be in 3D!", 100, 300, "normal 600 54px monospace", "red", 5);
+
+
+    function draw3DText(text, x, y, style, colour, size) {
+        context.font = style;
+        context.fillStyle = "black";
+
+        for (let i = 0; i < size; i++) {            
+            context.fillText(text, x - i, y - i);
+        }
+
+        context.fillStyle = colour;
+        context.fillText(text, x, y);
+    }
+}
