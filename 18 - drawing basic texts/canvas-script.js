1f57077d2c35a02e86489e4c66c1ab6520cb27a0
Elliot Moule <ellimyt@live.com.au>
2019-12-23 11:06:25 +1100

    [Basic Text Drawing]

---
+++ 18 - drawing basic texts/canvas-script.js
@@ -0,0 +1,20 @@
+
+window.onload = function () {
+    // Definitions
+    var canvas  = document.getElementById("basic-text-canvas");
+    var context = canvas.getContext("2d");
+
+    context.font = "45px Verdana";
+
+    // context.fillText(text, xCoord, yCoord, maxWidth);
+
+    var fText = "Fill Text on Canvas";
+    
+    context.fillText(fText, 80, 100);
+
+    // context.strokeText(text, xCoord, yCoord, maxWidth);
+
+    var sText = "Stroke Text on Canvas";
+
+    context.strokeText(sText, 80, 200);
+}
