8385f292611687d1af389e8996e6da8ce1a9fdf7
Elliot Moule <ellimyt@live.com.au>
2019-12-30 09:18:41 +1100

    [began bar chart library] - didn't push yesterday, so pushing now, as just updated license.

---
+++ 23 - project open source bar char library/bar.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ * bar.js
+ * simple, elegant, bar chart library
+ * 30/12/2019 - version 1.0
+ * https://github.com/elliotmoule/learn_html5_canvas
+ * 
+ * Copyright 2017 Elliot Moule
+ * Released under the MIT License
+ * https://github.com/elliotmoule/learn_html5_canvas/blob/master/LICENSE
+ * 
+ */
+
+// Constructor
+function BarChart(targetId, width, height, data) {
+    console.log(arguments);
+}
