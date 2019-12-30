253412a79ee33bb4b7094a9aecaab1c0f2c42a50
Elliot Moule <ellimyt@live.com.au>
2019-12-30 11:33:27 +1100

    [adding data handling and preoperations prior to drawing] - creating chart within the document. - storing canvas chart as part of 'chart' var. - populating labels and values within chart. - now determining the width and height of the axes. - label frequency for vertical and horizontal added.

--- 23 - project open source bar char library/bar.js
+++ 23 - project open source bar char library/bar.js
@@ -20,7 +20,12 @@ function BarChart(targetId, width, height, data) {
     
     // Specify Configuration
     chart.configureChart(targetId, width, height, data);
-}
+
+    // Pre Operations
+    chart.performPreOperations();
+
+    
+};
 
 BarChart.prototype.configureChart = function (targetId, width, height, data) {
     // Base
@@ -67,4 +72,72 @@ BarChart.prototype.setChartParameters = function () {
     // Guideline Configurations
     chart.guidelineColor = '#e5e5e5';
     chart.guidelineWidth = 0.5;
+};
+
+BarChart.prototype.performPreOperations = function () {
+    // Base
+    var chart = this;
+
+    // Create Canvas
+    chart.createCanvas();
+
+    // Get Data
+    chart.handleData();
+
+    // Prepare Data
+    chart.prepareData();
+};
+
+BarChart.prototype.createCanvas = function () {
+    // Base
+    var chart = this;
+
+    // Create Canvas
+    var canvas = document.createElement('canvas');
+    canvas.id = chart.id + '-' + Math.random();
+    canvas.width = chart.width;
+    canvas.height = chart.height;
+
+    // Append canvas to target container.
+    document.getElementById(chart.id).innerHtml = '';  // clear container
+    document.getElementById(chart.id).appendChild(canvas);  // add canvas to clean container
+
+    // Add canvas to chart object
+    chart.canvas = canvas;
+    chart.context = canvas.getContext('2d');
+};
+
+BarChart.prototype.handleData = function () {
+    // Base
+    var chart = this;
+
+    // Data sets
+    chart.labels = [];
+    chart.values = [];
+
+    // Handle Data
+    chart.data.forEach(function(item) {
+        chart.labels.push(item.label);
+        chart.values.push(item.value);
+    });
+
+};
+
+BarChart.prototype.prepareData = function () {
+    // Base
+    var chart = this;
+
+    // Global Variables
+    chart.itemsNum = chart.data.length;
+    chart.maxValue = Math.max.apply(null, chart.values);
+    chart.minValue = Math.min.apply(null, chart.values);
+
+    // Axe Specifications
+    chart.verticalAxeWidth = chart.height - 2 * chart.verticalMargin;       // bottom and top margins
+    chart.horizontalAxeWidth = chart.width - 2 * chart.horizontalMargin;    // left and right margins
+
+    // Label Specifications
+    chart.verticalUpperBound = Math.ceil(chart.maxValue / 10) * 10;
+    chart.verticalLabelFrequency = chart.verticalUpperBound / chart.itemsNum;
+    chart.horizontalLabelFrequency = chart.horizontalAxeWidth / chart.itemsNum;
 };
