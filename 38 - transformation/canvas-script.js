904b65e99645aba479b0d6a24f069461c70dcc99
Elliot Moule <ellimyt@live.com.au>
2020-01-06 15:32:43 +1100

    [Transformations: Transformation Matrix]

---
+++ 38 - transformation/canvas-script.js
@@ -0,0 +1,37 @@
+
+window.onload = function () {
+    // Definitions
+    var canvas  = document.getElementById("transformation-canvas");
+    var context = canvas.getContext("2d");
+
+    // context.transform(m11,m12,m22, dx, dy)
+    // context.setTransform(m11, m12, m21, m22, dx, dy)
+    // context.resetTransform()
+
+    // Red Rectangle
+    context.fillStyle = "red";
+    context.fillRect(150, 150, 200, 100);
+    context.fill();
+
+    context.transform(1.5, 0, 0, 1.5, 0, 0);
+
+    // Blue Rectangle
+    context.fillStyle = "blue";
+    context.fillRect(150, 150, 200, 100);
+    context.fill();
+
+    context.transform(1.5, 0, 0, 1.5, 0, 0);
+
+    // Green Rectangle
+    context.fillStyle = "green";
+    context.fillRect(150, 150, 200, 100);
+    context.fill();
+
+    context.resetTransform();
+
+    // Purple Rectangle
+    context.fillStyle = "purple";
+    context.fillRect(150, 150, 200, 100);
+    context.fill();
+
+};
