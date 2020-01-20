1622934c5bead59562d55d76a6b0ab96e294aef8
Elliot Moule <ellimyt@live.com.au>
2020-01-20 16:30:53 +1100

    [physics - velocity]

---
+++ 40 - physics velocity/canvas-script.js
@@ -0,0 +1,46 @@
+
+window.onload = function () {
+    // Definitions
+    var canvas  = document.getElementById("physics-intro-canvas");
+    var context = canvas.getContext("2d");
+
+    var ball = new Ball(30, 'red');
+    ball.x = 100;
+    ball.y = 150;
+    ball.context = context;
+    ball.draw();
+
+
+
+    window.requestAnimationFrame(animationLoop);
+
+    // Velocity
+    ball.vx = 5;
+    ball.vy = 5;
+
+    function animationLoop() {
+        // Clear Canvas
+        context.clearRect(0,0,canvas.width, canvas.height);
+
+        // Update
+        ball.x = ball.x + ball.vx;
+        ball.y = ball.y + ball.vy;
+
+        // Draw
+        ball.draw();
+
+        // Animate
+        window.requestAnimationFrame(animationLoop);
+    };
+
+    window.requestAnimationFrame = (function(){
+        return  window.requestAnimationFrame            ||
+                window.webkitRequestAnimationFrame      ||
+                window.mozRequestAnimationFrame         ||
+                window.msRequestAnimationFrame          ||
+                function(callback) {
+                    window.setTimeout(callback, 1000, 60);
+                };
+    })();
+
+};
